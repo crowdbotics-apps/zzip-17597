55fd23e1d2124b8d9cd28794913cd162a005d994
Crowdbotics <team@crowdbotics.com>
2020-05-30 22:43:31 +0000

    Initial commit.

---
+++ backend/zzip_17597/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zzip_17597 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zzip_17597.settings')
+
+application = get_wsgi_application()
